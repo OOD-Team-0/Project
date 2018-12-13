fda0864b1d71f5420438510da060cee5b23fa288
joedementri <joedementri42012@gmail.com>
2018-12-13 16:24:07 -0500

    Initial GUI code

--- memorymanager/Main.java
+++ memorymanager/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
         System.out.println("Hello World");
 
+        View v = new View();
+
         //To shut up the errors
         FirstFit fifo = new FirstFit();
         MemoryEvent me = new MemoryEvent();
--- memorymanager/View.java
+++ memorymanager/View.java
@@ -1,17 +1,86 @@
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class View implements Observer {
+
+    private JFrame main;
+    private JMenuBar menubar;
 
     /**
      * Constructor for the View class.
      */
     public View() {
 
+        //Make sure the look and feel is set to windows
+        try
+        {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+        }
+        catch(Exception e){
+        }
+        //Initialize everything
+        initComponents();
+
     }
 
     @Override
     public void update(Observable o, Object arg) {
 
+    }
+
+    private void initComponents() {
+
+        //Construct the JFrame
+        main = new JFrame("Memory Manager");
+
+        //Create a Container and set the layout to BoxLayout (Top->Bottom)
+        Container pane = main.getContentPane();
+        pane.setLayout(new BoxLayout(pane, BoxLayout.PAGE_AXIS));
+
+        //Go Create a Menu Bar
+        createMenuBar(main);
+
+
+        ImageIcon img = new ImageIcon("memorymanager/ram.png");
+        main.setIconImage(img.getImage());
+        main.pack();
+        //Show it
+        main.setVisible(true);
+
+
+    }
+
+    private void createMenuBar(JFrame main) {
+
+        //Create a menubar and add it to Frame
+        menubar = new JMenuBar();
+        main.setJMenuBar(menubar);
+
+        JMenu menu;
+        JMenuItem item;
+
+        //Create file menu
+        menu = new JMenu("File");
+        menubar.add(menu);
+
+        item = new JMenuItem("Run Simulation");
+        item.setMnemonic('R');
+        menu.add(item);
+
+        menu.addSeparator();
+
+        item = new JMenuItem("Quit");
+        item.setMnemonic('Q');
+        menu.add(item);
+
+        menu = new JMenu("Help");
+        menubar.add(menu);
+
+        item = new JMenuItem("About");
+        item.setMnemonic('H');
+        menu.add(item);
     }
 }
