9da19f736da2322d91928cc44ff0318f0271f5af
topalisd8 <topalisd8@students.rowan.edu>
2018-12-19 17:37:02 -0500

    cleand up main

--- memory_manager/ood_main.py
+++ memory_manager/ood_main.py
@@ -4,22 +4,20 @@ from ood_view import View
 from ood_memory_manager import MemoryManager
 from ood_first_fit import FirstFit
 from ood_fake_scheduler import FakeScheduler
-from tkinter import Tk
+
 
 def main():
 
-
-    memorySize = 100
+    memorySize = 100    # Number of indexes in the main memory array
     sc = SimulationController()
 
-    ff = FirstFit()
-    mm = MemoryManager(memorySize)     # Controller can probably change this
+    ff = FirstFit()     # Memory Management Algorithm
+    mm = MemoryManager(memorySize)
     mm.setMMA(ff)
-    fs = FakeScheduler()
+    fs = FakeScheduler()    # Scheduler
+    v = View(memorySize)    # View
+    os = OS()               # OS
 
-    v = View(memorySize)
-
-    os = OS()
     os.setMemoryManager(mm)
     os.setScheduler(fs)
     os.addObserver(v)
