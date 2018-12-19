4c5af38cecee2525a045ac815b44ec6f4d804281
finnion285 <43827744+finnion285@users.noreply.github.com>
2018-12-18 19:17:20 -0500

    Update FakeScheduler.java

--- memorymanager/FakeScheduler.java
+++ memorymanager/FakeScheduler.java
@@ -1,16 +1,24 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Random;
 
-public class FakeScheduler implements Scheduler{
+public class SchedulerFake implements Scheduler{
     private final int MIN_SIZE = 1;     //Number of indexes of the memory array it needs
     private final int MAX_SIZE = 20;
     private final int MIN_TIME = 5;     //In seconds
     private final int MAX_TIME = 20;
+    private final int MAX_RUN_TIME = 100;
+    private int timeCounter;
+    private Random rand;
 
     private ArrayList<Process> processes = new ArrayList<>();
     private int processNumber = 1;
 
-    public FakeScheduler() { }
+    public SchedulerFake()
+    {
+        timeCounter = 0;
+        rand = new Random();
+    }
 
     @Override
     public ArrayList<Process> getProcesses() {
@@ -19,12 +27,9 @@ public class FakeScheduler implements Scheduler{
         return temp;
     }
 
-    private void generateProcesses() {
-        int p = 1 + (int)(Math.random() * (3 - 1));
-        for(int i = 0; i < p; i++) {
-            Process process = new Process(processNumber++, this.generateProcessTime(), this.generateProcessSize());
-            this.processes.add(process);
-        }
+    private void generateProcess() {
+        Process process = new Process(processNumber++, this.generateProcessTime(), this.generateProcessSize());
+        this.processes.add(process);
     }
 
     private int generateProcessSize() {
@@ -33,6 +38,27 @@ public class FakeScheduler implements Scheduler{
 
     private int generateProcessTime() {
         return MIN_TIME + (int)(Math.random() * (MAX_TIME - MIN_TIME));
+    }
+
+    while(timeCounter < MAX_RUN_TIME)
+    {
+        MIN_TIME + (int)(Math.random() * (MAX_TIME - MIN_TIME);
+        if(!(rand.nextBoolean()) && processes.size() > 0))
+        {
+            event = generateEndEvent(processes.size());
+            processes.remove(event.getProcess());
+        }
+        else
+        {
+            event = generateStartEvent();
+            processes.add(event.getProcess());
+        }
+        try
+        {
+            wait(1000);
+        }
+        catch (Exception e){};
+        timeCounter++;
     }
 }
 
