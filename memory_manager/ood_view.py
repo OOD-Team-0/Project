41cdff7bef12b487a286d10566fc63d5193579eb
joedementri <joedementri42012@gmail.com>
2018-12-19 06:49:19 -0500

    GUI Code initial

--- memory_manager/ood_view.py
+++ memory_manager/ood_view.py
@@ -1,34 +1,27 @@
-
+from tkinter import *
+from ood_observer import Observer
 from random import randint
 
-class View:
+class View(Observer):
     # Should be made into GUI
-    def __init__(self):
-        self.bar = []
-
-    def setBarSize(self, size):
+    def __init__(self, size):
         self.bar = [x*0 for x in range(size)]
+        self.gui = Tk()
+        self.gui.title("Memory Manager")
+        self.gui.update()
 
     def update(self, me):
-        print('Pid:{}, Is:{}'.format(me.pid, me.indexes))
+
+        added = False
         for i in me.indexes:
             if(self.bar[i] == me.pid):
                 self.bar[i] = 0
             else:
                 self.bar[i] = me.pid
+                added = True
+        if(added):
+            s = 'Added'
+        else:
+            s = 'Removed'
+        print('Pid:{}, {}:{}'.format(me.pid, s ,me.indexes))
         print(self.bar)
-
-
-#def randomAction(a):
-    #searching = True
-    #while(searching):
-        #i = randint(0,3)
-        #if(a[i] != None):
-            #searching = False
-    #return i
-
-#b = [1,1,None,1]
-
-#for i in range(50):
-   # print(randomAction(b))
-    
