93d85223fccd694fb323023bc28d4c5bb2f5780a
finnion285 <43827744+finnion285@users.noreply.github.com>
2018-12-19 20:38:52 -0500

    Further streamlined documentation

--- memory_manager/ood_scheduler.py
+++ memory_manager/ood_scheduler.py
@@ -2,8 +2,8 @@ from abc import ABC, abstractmethod
 
 class Scheduler(ABC):
     """"
-		Interface for an OS Scheduler. 
-		:param	ABC
+    Interface for an OS Scheduler.
+    :param	ABC
     """
     @abstractmethod
     def getProcessEvents(self): pass
